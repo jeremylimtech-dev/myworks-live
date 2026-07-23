b0e46cb7e8596fcb606f8d66bab2f00e424c4d1e
Jeremy Lim <jeremylimtech@gmail.com>
2026-07-23 09:55:55 +0800

    Fix prisma schema url definition

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -6,12 +6,6 @@ const globalForPrisma = globalThis as unknown as {
 
 export const prisma =
   globalForPrisma.prisma ??
-  new PrismaClient({
-    datasources: {
-      db: {
-        url: process.env.DATABASE_URL || "mysql://root:@localhost:3306/myworks",
-      },
-    },
-  });
+  new PrismaClient();
 
 if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
